d09bd9504b55deeb77c270f7b90c1b947a5f96c6
Luna <luna@Moon>
2017-11-01 11:59:01 +0700

    Fix missing dependencies for freezing

--- setup.py
+++ setup.py
@@ -1,17 +1,16 @@
 from cx_Freeze import setup, Executable
 import requests
-import sys
 
-buildOptions = {"packages": ["os", 'requests', 'queue'],
-                'includes': ['requests', 'dermod', 'queue'],
+buildOptions = {"packages": ["os", 'requests', 'queue', 'idna'],
+                'includes': ['requests', 'dermod', 'queue', 'idna'],
                 "excludes": ["tkinter"],
                 'include_files': ["extra/", (requests.certs.where(), 'cacert.pem')]
-}
+                }
 
 setup(
-    name = "DBooru",
-    version = "1.0.0",
-    requires = 'requests',
-    options = {"build_exe": buildOptions},
-    executables = [Executable("main.py"), Executable('webv3.py')]
+    name="DBooru",
+    version="1.0.0",
+    requires=['requests', 'idna'],
+    options={"build_exe": buildOptions},
+    executables=[Executable("main.py"), Executable('webv3.py')]
 )
