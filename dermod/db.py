43c52e1e05cd847e578b3c3b476bc1fb6e907b1c
Luna <luna@Moon>
2017-11-08 19:25:45 +0700

    Fix

--- dermod/db.py
+++ dermod/db.py
@@ -1,6 +1,7 @@
 import os
 import sqlite3
 import sys
+import re
 
 from dermod import input_parser as ip
 import settings_file
@@ -161,14 +162,15 @@ def special_f(specials):
             else:
                 pass
         i = i.split(splitter)
+        value = i[1].replace("*", "%")
         if i[0] == 'height':
-            results = src(i[1], i[0], splitter)
+            results = src(value, i[0], splitter)
             conn.commit()
         elif i[0] == 'width':
-            results = src(i[1], i[0], splitter)
+            results = src(value, i[0], splitter)
             conn.commit()
         elif i[0] == 'ratio' or i[0] == 'aspect_ratio':
-            results = src(i[1][:10], 'ratio', splitter)
+            results = src(value[:10], 'ratio', splitter)
             conn.commit()
     if len(results) == 0:
         results = []
