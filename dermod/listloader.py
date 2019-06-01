8d1d9b58f2e87cf49d7075f15786cb749ed6cc2b
mcilya <i.tretyakov@mcilya.xyz>
2019-06-01 14:18:17 +1000

    Handle pg_num argument for module().parse()

--- dermod/listloader.py
+++ dermod/listloader.py
@@ -61,7 +61,10 @@ class Checker(Thread):
             self.raw_data = self.raw_data.content.decode()
 
     def parse_data(self):
-        self.module_data.parse(string=self.raw_data, pg_num=self.page)
+        try:
+            self.module_data.parse(string=self.raw_data, pg_num=self.page)
+        except TypeError:
+            self.module_data.parse(string=self.raw_data)
 
     def compile(self):
         digest = str(sha384(self.module.__name__.encode()).hexdigest())[
